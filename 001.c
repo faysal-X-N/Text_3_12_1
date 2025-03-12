e9a416a13a8ac8d8dca77c4c934d21e4dde8cef1
faysal <yunxin.boy@qq.com>
2025-03-12 18:20:58 +0800

    002

--- 001.c
+++ 001.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-int main()
-{
-	//判断一个数是否为奇数
-	int num = 0;
-	scanf_s("%d", &num);
-	if(num%2==1)
-	{
-		printf("奇数\n");
-	}
-	else
-	{
-		printf("NO\n");
-	}
-
-	return 0;
-}
+//int main()
+//{
+//	//判断一个数是否为奇数
+//	int num = 0;
+//	scanf_s("%d", &num);
+//	if(num % 2 == 1)
+//	{
+//		printf("奇数\n");
+//	}
+//	else
+//	{
+//		printf("NO\n");
+//	}
+//
+//	return 0;
+//}
---
+++ 002.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	//输入1-100之间的奇数
+	int num = 0;
+	int num1 = 0;
+	while (num <=100)
+	{
+		if (num % 2 == 1)
+		{
+
+			printf("奇数：%d\n", num);
+		}
+		
+		num++;
+	}//输出0-100的偶数
+	while (num1 <= 100)
+	{
+		if (num1 % 2 == 0)
+		{
+
+			printf("偶数：%d\n", num1);
+		}
+
+		num1++;
+	}
+	
+
+
+	return 0;
+}
